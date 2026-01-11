d32a3e51a34c2055296824671a2116fb8c0ecdf4
BasnaYanti <basna.yanti.ft24@mail.umy.ac.id>
2026-01-11 16:35:57 +0700

    yy

--- app/config/database.js
+++ app/config/database.js
@@ -1,20 +1,16 @@
-// TODO: Buat koneksi pool MySQL disini menggunakan Environment Variable (process.env)
-
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,     // db
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'siapprodi',
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect(err => {
-  if (err) {
-    console.error('Database error:', err);
-  } else {
-    console.log('Database connected');
-  }
-});
-
-module.exports = db;
+// Kita ekspor versi promise agar bisa pakai async/await di routes
+module.exports = pool.promise();
